bb4e8f9ed56461ee1d2dcd2355915a07820e419f
Kanishk Gandhi <kanishk050296@gmail.com>
2025-10-22 17:13:12 -0700

    debug

--- vllm/v1/sample/sampler.py
+++ vllm/v1/sample/sampler.py
@@ -149,27 +149,27 @@ class Sampler(nn.Module):
         may update the logits tensor in-place.
         """
 
-        # assert not (sampling_metadata.all_greedy and sampling_metadata.all_random)
-        # if sampling_metadata.all_random:
-        #     greedy_sampled = None
-        # else:
-        #     print("greedy sampling")
-        #     greedy_sampled = self.greedy_sample(logits)
-        #     if sampling_metadata.all_greedy:
-        #         processed_logprobs = None
-        #         if sampling_metadata.max_num_logprobs is not None:
-        #             if self.logprobs_mode == "processed_logits":
-        #                 processed_logprobs = logits
-        #             elif self.logprobs_mode == "processed_logprobs":
-        #                 processed_logprobs = self.compute_logprobs(logits)
-        #         return greedy_sampled, processed_logprobs
+        assert not (sampling_metadata.all_greedy and sampling_metadata.all_random)
+        if sampling_metadata.all_random:
+            greedy_sampled = None
+        else:
+            print("greedy sampling")
+            greedy_sampled = self.greedy_sample(logits)
+            if sampling_metadata.all_greedy:
+                processed_logprobs = None
+                if sampling_metadata.max_num_logprobs is not None:
+                    if self.logprobs_mode == "processed_logits":
+                        processed_logprobs = logits
+                    elif self.logprobs_mode == "processed_logprobs":
+                        processed_logprobs = self.compute_logprobs(logits)
+                return greedy_sampled, processed_logprobs
 
-        # assert sampling_metadata.temperature is not None
+        assert sampling_metadata.temperature is not None
 
         # Apply temperature.
-        # logits = self.apply_temperature(
-        #     logits, sampling_metadata.temperature, sampling_metadata.all_random
-        # )
+        logits = self.apply_temperature(
+            logits, sampling_metadata.temperature, sampling_metadata.all_random
+        )
 
 
         
@@ -210,20 +210,17 @@ class Sampler(nn.Module):
         )
 
         
-        sampled = self.greedy_sample(logits)
-        print("aaaaa sampled")
-        return sampled, logits
-        # import pdb; pdb.set_trace()
-        # if greedy_sampled is None:
-        #     return random_sampled, processed_logprobs
+        print("aaaa sampled")
+        if greedy_sampled is None:
+            return random_sampled, processed_logprobs
 
-        # sampled = torch.where(
-        #     sampling_metadata.temperature < _SAMPLING_EPS,
-        #     greedy_sampled,
-        #     random_sampled,
-        #     out=greedy_sampled,  # Reuse tensor
-        # )
-        # return sampled, processed_logprobs
+        sampled = torch.where(
+            sampling_metadata.temperature < _SAMPLING_EPS,
+            greedy_sampled,
+            random_sampled,
+            out=greedy_sampled,  # Reuse tensor
+        )
+        return sampled, processed_logprobs
 
     @staticmethod
     def compute_logprobs(logits: torch.Tensor) -> torch.Tensor:
